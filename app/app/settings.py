033eefc9edbb8254d0e32c8f6c0b8554dfbef0bb
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2025-06-04 01:55:35 +0600

    Deploy changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-!#7!fr-wcaj_eaij!j&5nkqz!o#ef0cu+t+@*cbw^0zz)ngte$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
