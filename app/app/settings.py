0ae0d44e33c3ab4c8083481fcdb27850d295e479
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2025-06-06 02:24:02 +0600

    Deploy test2.0

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-!#7!fr-wcaj_eaij!j&5nkqz!o#ef0cu+t+@*cbw^0zz)ngte$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
