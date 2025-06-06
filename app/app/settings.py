14a1a5ca536a3dc4c1d1e916038d889bf65efc1f
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2025-06-06 18:29:09 +0600

    deploy test3.0

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+# new for deploy
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +87,7 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": "1234",
         "HOST": "localhost",
+        
     }
 }
 
